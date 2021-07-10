fc7310a3529192d45b6e45c6f68f7b3f60b4a117
고유림 <qpeijty1@naver.com>
2021-07-10 15:58:38 +0900

    print hello world test success

---
+++ src/main/java/com/exe/HelloWorld/Controllers/HelloController.java
@@ -0,0 +1,14 @@
+package com.exe.HelloWorld.Controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ src/main/java/com/exe/HelloWorld/MainClass.java
@@ -0,0 +1,12 @@
+package com.exe.HelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+public class MainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class,args);
+    }
+}
